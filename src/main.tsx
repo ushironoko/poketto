8967748865cf0069c5e6cd96c015182ebfb3eb62
ushironoko <apple19940820@gmail.com>
2020-04-22 13:30:41 +0900

    いらんもん消す

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import { h } from '~/helpers/h'
 import { createApp, defineComponent, App } from 'vue'
-import { route, routerHistory } from '~/router'
-declare global {
-  interface Window {
-    // h: HTML5History
-    h: typeof routerHistory
-    r: typeof route
-    vm: ReturnType<App['mount']>
-  }
-}
+import { route } from '~/router'
 
 const App = defineComponent({
   name: 'App',
